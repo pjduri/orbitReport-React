e07dcad2dbef52cbba5cee29363a483e788bfb79
Your Name <108307350+pjduri@users.noreply.github.com>
2024-05-08 20:12:50 -0500

    added stylesheet to components

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,11 +1,12 @@
 import satData from './satData'
+import './styling.css'
 
 const Buttons = ({ filterByType, setSat, allOrbitTypes }) => {
 
   return (
-    <div>
+    <div className='flex-container'>
       {allOrbitTypes.map((type, idx) => 
-          <button onClick={() => filterByType(type)} key={idx}>{type}</button>)}
+          <button onClick={() => filterByType(type)} key={idx}>{type} Orbit</button>)}
       <button onClick={()=>{setSat(satData)}}>All Orbits</button>
     </div>
   )
