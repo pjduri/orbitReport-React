cd0326fbb702fc0f6732447695adc1be1f217d4a
Your Name <108307350+pjduri@users.noreply.github.com>
2024-05-08 19:48:29 -0500

    sat type filtering function complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
 import Buttons from './components/Buttons'
 import Table from './components/Table'
 import Banner from './components/Banner'
+import { useState, useEffect } from 'react'
+import satData from './components/satData'
 
 
 function App() {
-  return (
+
+  const [sat, setSat] = useState(satData)
+  const allOrbitTypes = [... new Set(satData.map(data => data.orbitType))]
+
+  
+  
+  const filterByType = (currentType) => {
+    const satsOfType = satData.filter(satellite => 
+      satellite.orbitType === currentType)
+      
+    setSat(satsOfType)
+  }
+    
+    return (
     <div>
       <Banner />
-      <Buttons />
-      <Table />
+      <Buttons
+        filterByType={filterByType}
+        setSat={setSat}
+        allOrbitTypes={allOrbitTypes} />
+      <Table sat={sat} />
     </div>
   )
 }
