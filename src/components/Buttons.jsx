fb95c562d10476e34fc653158886d2845f8782da
Your Name <108307350+pjduri@users.noreply.github.com>
2024-05-09 09:32:25 -0500

    final edits

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -6,7 +6,10 @@ const Buttons = ({ filterByType, setSat, allOrbitTypes }) => {
   return (
     <div className='flex-container'>
       {allOrbitTypes.map((type, idx) => 
-          <button onClick={() => filterByType(type)} key={idx}>{type} Orbit</button>)}
+          <button onClick={() => filterByType(type)} 
+            key={idx}>
+              {type} Orbit
+          </button>)}
       <button onClick={()=>{setSat(satData)}}>All Orbits</button>
     </div>
   )
