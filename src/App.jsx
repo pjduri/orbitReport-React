ab2bab7e8ee8aa66d2c9589f3d041747e0d33c40
Your Name <108307350+pjduri@users.noreply.github.com>
2024-05-08 19:26:04 -0500

    banner component done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import Buttons from "./components/Buttons";
-import Table from "./components/Table";
+import Buttons from './components/Buttons'
+import Table from './components/Table'
+import Banner from './components/Banner'
 
 
 function App() {
   return (
     <div>
+      <Banner />
       <Buttons />
       <Table />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Banner.jsx
@@ -0,0 +1,9 @@
+const Banner = () =>
+    <>
+        <header>
+            <h1>Orbit Report</h1>
+        </header>
+        <p>Click on the buttons to see the satellites in that orbit type</p>
+    </>
+
+export default Banner
