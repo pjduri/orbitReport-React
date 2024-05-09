57307e89cb926913abb13cb3f8e82f7e00834d8a
Your Name <108307350+pjduri@users.noreply.github.com>
2024-05-08 20:02:52 -0500

    Table component displays filtered satellite data

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -3,13 +3,21 @@ const Table = ({ sat }) => {
     <table>
     <thead>
      <tr>
-       <th>Header TBD</th>
+       <th>Name</th>
+       <th>Type</th>
+       <th>Launch Date</th>
+       <th>Status</th>
      </tr>
      </thead>
      <tbody>
-     <tr>
-       <td>Row Data TBD</td>
-     </tr>
+     {sat.map(satellite => (
+      <tr key={satellite.id}>
+        <td>{satellite.name}</td>
+        <td>{satellite.type}</td>
+        <td>{satellite.launchDate}</td>
+        <td>{satellite.operational ? 'active' : 'inactive'}</td>
+      </tr>
+     ))}
      </tbody>
    </table>
   );
